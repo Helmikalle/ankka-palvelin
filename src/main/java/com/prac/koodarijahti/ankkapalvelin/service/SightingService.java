dfbcb45d3f3d579c641e8e2263253182c1bdc1ae
Kalle <kalhelm@hotmail.com>
2018-03-21 14:07:00 +0200

    Fixed when adding new sighting it automagically gives next free database id instead of asking if there's id or not.

--- src/main/java/com/prac/koodarijahti/ankkapalvelin/service/SightingService.java
+++ src/main/java/com/prac/koodarijahti/ankkapalvelin/service/SightingService.java
@@ -24,7 +24,6 @@ public class SightingService {
             if (sightings.getDateTime() == null || sightings.getDateTime().equals(""))
             sightings.setDateTime(new Date());
             sightings.setId(sightingsRepository.count() + 1);
-//            if (sightingsRepository.existsById(sightings.getId())) sightings.setId(sightingsRepository.count() + 1);
             return sightingsRepository.save(sightings);
         } else throw new CustomNotFoundException("No such bird in the database - " + sightings.getSpecies().getName());
       }
